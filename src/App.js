bca7cd1a58e76094072be2fbd690e26a4aafef4c
Tyler Smith <tylersmithlf@gmail.com>
2023-03-11 10:45:38 -0700

    Comments out video, switches form

--- src/App.js
+++ src/App.js
@@ -27,10 +27,10 @@ export default function AgentLandingPage() {
         //   backgroundSize: 'cover'
         // }}
       >
-        <div className='bg-light-gray py-5'>
-          <div className='flex justify-between items-center lg:mx-16 mx-3 pt-8 mb-4'>
+        <div className='bg-light-gray py-3'>
+          <div className='flex justify-between items-center lg:mx-16 mx-3 pt-5 mb-4'>
             <p className='text-3xl font-extrabold tracking-tight'>🌴DR Trips</p>
-            <a href="https://notionforms.io/forms/agents-beta-signup" target="__blank" className='flex justify-center hover:no-underline'>
+            <a href="https://forms.gle/AQ4fv3vkMVr5GjL28" target="__blank" className='flex justify-center hover:no-underline'>
               <button style={{borderWidth: '1px'}} className='px-5 py-3 text-xl text-bot-blue bg-light-bot-blue-0 hover:bg-light-bot-blue font-semibold rounded-xl'>Sign up</button>
             </a>
           </div>
@@ -44,17 +44,18 @@ export default function AgentLandingPage() {
               <div className='flex justify-center mb-5 lg:mx-5'>
                 <p className='lg:text-2xl text-xl mt-6 text-center lg:leading-10 leading-8'>We're helping 300+ students in the Dominican Republic get an education <span className='font-semibold'>and you can help us</span>.</p>
               </div>
-              <a href="https://notionforms.io/forms/agents-beta-signup" target="__blank" className='flex justify-center mt-10 my-4 hover:no-underline'>
+              <a href="https://forms.gle/AQ4fv3vkMVr5GjL28" target="__blank" className='flex justify-center mt-10 my-4 hover:no-underline'>
                 <button className='px-10 py-4 text-xl bg-bot-blue hover:bg-dark-bot-blue text-white font-semibold rounded-xl'>Volunteer form</button>
               </a>
             </div>
           </div>
 
-          <div className='py-3 mx-2'>
+          {/* <div className='py-3 mx-2'>
             <div className='flex justify-center'>
               <iframe style={{ width: '1000px', height: isMobile ? '300px' : '500px' }} className='rounded-xl lg:shadow-xl shadow-gray cursor-pointer' src="https://www.youtube.com/embed/fLFw1Jjp01I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
-          </div>
+          </div> */}
+
         </div>
 
         <div className='flex justify-center items-center'>
@@ -90,7 +91,7 @@ export default function AgentLandingPage() {
                 <div className='flex justify-start items-center gap-4'>
                   <p className='ml-2 lg:text-5xl mt-2 text-4xl text-bot-blue font-extrabold'>01</p>
                   <p
-                    style={{width: '200px'}}
+                    style={{width: isMobile ? '200px' : '300px'}}
                     className='bg-light-bot-blue-0 py-2 mt-2 text-bot-blue font-bold lg:text-3xl text-2xl rounded-xl text-center shadow-md shadow-light-gray-3'
                   >
                     Build cirriculum
@@ -104,7 +105,7 @@ export default function AgentLandingPage() {
               <div className='flex justify-start items-center gap-4'>
                   <p className='ml-2 lg:text-5xl mt-2 text-4xl text-bot-blue font-extrabold'>02</p>
                   <p
-                    style={{width: '200px'}}
+                    style={{width: isMobile ? '200px' : '300px'}}
                     className='bg-light-bot-blue-0 py-2 mt-2 text-bot-blue font-bold lg:text-3xl text-2xl rounded-xl text-center shadow-md shadow-light-gray-3'
                   >
                     Teach students
@@ -116,16 +117,24 @@ export default function AgentLandingPage() {
           </div>
         </div>
 
+        <div id="how-it-works" className='py-20'>
+          <p className={`text-center lg:text-4xl text-3xl font-extrabold tracking-tight ${isMobile ? 'mt-5 mb-16' : 'mb-32'}`}>FAQ</p>
+        </div>
+        
+        <div id="how-it-works" className='py-24'>
+          <p className={`text-center lg:text-4xl text-3xl font-extrabold tracking-tight ${isMobile ? 'mt-5 mb-16' : 'mb-32'}`}>Contact form here</p>
+        </div>
+
       </div>
 
-      <div className={`flex justify-center items-center pb-16 bg-light-gray ${isMobile ? 'px-3' : ''}`} id="beta">
-        <div className='px-4 py-5 -mt-20 bg-light-bot-blue-0 lg:w-8/12 rounded-xl shadow shadow-light-gray-3'>
+      <div className={`flex justify-center items-center pb-16 ${isMobile ? 'px-3' : ''}`} id="beta">
+        <div className='lg:px-5 px-3 py-8 -mt-20 bg-light-bot-blue-0 lg:w-8/12 rounded-xl shadow shadow-light-gray-3'>
           <div className='lg:flex lg:justify-between lg:mr-12'>
             <div className={isMobile ? '' : 'ml-3'}>
               <p className='lg:text-4xl text-3xl text-black tracking-tight font-extrabold mb-2 text-center-mobile'>Join the crew</p>
               <p className='font-gray text-xl text-center-mobile'>Help us develop cirriculum, teach students, and build the program.</p>
             </div>
-            <a href="https://notionforms.io/forms/agents-beta-signup" target="__blank" className='flex justify-center hover:no-underline'>
+            <a href="https://forms.gle/AQ4fv3vkMVr5GjL28" target="__blank" className='flex justify-center hover:no-underline'>
               <button
                 style={{width: isMobile ? '100%' : ''}}
                 className={`px-5 py-3 text-xl text-white bg-bot-blue hover:bg-dark-bot-blue font-semibold rounded-xl ${isMobile ? 'mt-4 -mb-2' : ''}`}
