12629ba352864c1baea8d4187365cb38847adda9
Tyler Smith <tylersmithlf@gmail.com>
2023-03-25 13:14:39 -0600

    Adds testimonials and slideshow

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import teachers from './assets/teachers.png';
 import emailjs from 'emailjs-com';
 import AccordianContent from './AccordianContent';
 import { Menu, MenuItem } from "@mui/material";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 export default function LandingPage() {
 
@@ -52,6 +54,7 @@ export default function LandingPage() {
   const [dateSelected, setDateSelected] = useState(datesCost.length[`${length}`].dates[0]);
   const [dates, setDates] = useState(datesCost.length[`${length}`].dates);
   const [cost, setCost] = useState(datesCost.length[`${length}`].cost);
+  const [testimonial, setTestimonial] = useState(1);
   
   const notifyMessageSent = (msg) => toast.success(<p>{msg}</p>, {
     toastId: 'messageSent',
@@ -76,8 +79,8 @@ export default function LandingPage() {
       "thisKey": "02"
     },
     {
-      "question": "How safe is it?",
-      "answer": "Moderately. Recommend not wandering around alone. Recommend not going out at night. ",
+      "question": "Is it safe?",
+      "answer": "While the area is generally safe, it's always best to take precautions when traveling. It's recommended to avoid going out alone, especially at night. By taking these simple measures, you can help ensure your safety and enjoy your time here with peace of mind.",
       "thisKey": "03"
     },
     {
@@ -219,6 +222,18 @@ export default function LandingPage() {
     setAnchorElDates(event.currentTarget);
   };
 
+  const handleTestimonial = (add) => {
+    if (add && testimonial === 3) {
+      setTestimonial(1);
+    } else if (add) {
+      setTestimonial(testimonial + 1);
+    } else if (add === false && testimonial === 1) {
+      setTestimonial(3);
+    } else {
+      setTestimonial(testimonial - 1);
+    }
+  }
+
   
   return (
     <div>
@@ -262,7 +277,7 @@ export default function LandingPage() {
         </div>
 
         <div className='flex justify-center items-center'>
-          <div className='lg:flex lg:justify-center lg:items-center lg:gap-20 lg:py-32 pt-28 mx-3'>
+          <div className='lg:flex lg:justify-center lg:items-center lg:gap-20 lg:py-32 pt-12 mx-3'>
             <div className={isMobile ? 'mt-5' : ''} style={{width: isMobile ? '100%' : '500px'}}>
               <p className={`${isMobile ? 'text-4xl' : 'text-5xl'} font-extrabold tracking-tight`}>Our <span className='text-[#378805]'>Mission</span></p>
               <p className='lg:text-2xl text-xl mt-6 lg:leading-10 leading-8 mt-4'>Lift the rising generation by delivering and improving their education.</p>
@@ -292,13 +307,14 @@ export default function LandingPage() {
           </div>
         </div>
 
-        <div className='mx-2 mt-48'>
+        <div className='mx-2 lg:mt-48 mt-36'>
+          <p className={`${isMobile ? 'text-4xl' : 'text-5xl'} text-center font-extrabold tracking-tight mb-5`}>Here's our <span className='text-[#378805]'>why</span></p>
           <div className='flex justify-center'>
             <iframe style={{ width: '1000px', height: isMobile ? '250px' : '600px' }} className='rounded-xl lg:shadow-xl shadow-gray cursor-pointer' src="https://www.youtube.com/embed/DMwY99VKHWA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
         </div>
 
-        <div id="how-it-works" className='pt-32 lg:pb-44 pb-24'>
+        <div id="how-it-works" className='py-20'>
           <p className={`text-center lg:text-4xl text-3xl font-extrabold tracking-tight mt-5 ${isMobile ? 'mb-16' : 'mb-32'}`}>What will I be doing?</p>
           <div className={isMobile ? 'mx-5' : ''}>
             <div className={`lg:flex lg:justify-center lg:items-center lg:gap-32 mt-16`}>
@@ -374,9 +390,9 @@ export default function LandingPage() {
           </div>
         </div>
 
-        <div id="dates-cost-estimate" className='pb-28'>
+        <div id="dates-cost-estimate" className='py-20'>
           <p className={`text-center lg:text-4xl text-3xl font-extrabold tracking-tight my-5 mx-3`}>Dates & Cost Estimator</p>
-          <div className='lg:flex lg:justify-center lg:items-center lg:gap-10 pt-12 pb-20'>
+          <div className='lg:flex lg:justify-center lg:items-center lg:gap-10 pt-8 pb-20'>
             <div className='flex flex-col justify-center items-center'>
               <p className='text-center text-3xl font-semibold'>Length</p>
               <p
@@ -450,7 +466,7 @@ export default function LandingPage() {
           </div>
         </div>
 
-        <div id="how-it-works" className='pb-20'>
+        <div id="how-it-works" className='py-16'>
           <p className={`text-center lg:text-4xl text-3xl font-extrabold tracking-tight my-5 mx-3`}>Frequently Asked Questions</p>
           <Accordion className='flex flex-col gap-4 justify-center items-center'>
             {FAQs.map(function (faq, index) {
@@ -469,10 +485,61 @@ export default function LandingPage() {
             <p onClick={() => setShowMore(!showMore)} className='font-bold text-xl text-[#378805] cursor-pointer underline'>Show {showMore ? 'less' : 'more'}</p>
           </div>
         </div>
+
+        {/* Testimonials */}
+        <div className='py-3 mx-2 my-20'>
+          <p className={`text-center lg:text-4xl text-3xl font-extrabold tracking-tight my-5 mx-3`}>Tesimonial videos</p>
+          <div className='lg:flex lg:justify-center lg:items-center lg:gap-3'>
+            <FontAwesomeIcon icon={faArrowLeft} onClick={() => handleTestimonial(false)} className={isMobile ? 'hidden' : 'text-4xl px-3 py-5 rounded-xl cursor-pointer bg-light-gray hover:bg-light-gray-2'} />
+            <div>
+              <div className={testimonial === 3 ? 'flex justify-center' : 'hidden'}>
+                <iframe style={{ width: '900px', height: isMobile ? '224px' : '494px' }} className='rounded-xl lg:shadow-xl shadow-gray cursor-pointer' src="https://www.youtube.com/embed/38eVwuzq9mM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+              <div className={testimonial === 2 ? 'flex justify-center' : 'hidden'}>
+                <iframe style={{ width: '900px', height: isMobile ? '224px' : '494px' }} className='rounded-xl lg:shadow-xl shadow-gray cursor-pointer' src="https://www.youtube.com/embed/xdmEIpduM9c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+              <div className={testimonial === 1 ? 'flex justify-center' : 'hidden'}>
+                <iframe style={{ width: '900px', height: isMobile ? '224px' : '494px' }} className='rounded-xl lg:shadow-xl shadow-gray cursor-pointer' src="https://www.youtube.com/embed/9VTQvGMVezA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+            </div>
+            <FontAwesomeIcon icon={faArrowRight} onClick={() => handleTestimonial(true)} className={isMobile ? 'hidden' : 'text-4xl px-3 py-5 rounded-xl cursor-pointer bg-light-gray hover:bg-light-gray-2'} />
+            <div className={isMobile ? 'flex justify-around' : 'hidden'}>
+              <FontAwesomeIcon icon={faArrowLeft} onClick={() => handleTestimonial(false)} className='text-4xl px-5 py-2 mt-3 rounded-xl cursor-pointer bg-light-gray hover:bg-light-gray-2' />
+              <FontAwesomeIcon icon={faArrowRight} onClick={() => handleTestimonial(true)} className='text-4xl px-5 py-2 mt-3 rounded-xl cursor-pointer bg-light-gray hover:bg-light-gray-2' />
+            </div>
+          </div>
+        </div>
+
+        <div className={`flex justify-center items-center pt-40 pb-16 ${isMobile ? 'px-3' : ''}`} id="beta">
+          <div className='lg:px-5 px-3 py-8 -mt-20 bg-[#a3eb76]-0 lg:w-8/12 rounded-xl shadow shadow-light-gray-3 bg-light-gray'>
+            <div className='lg:flex lg:justify-between lg:mr-12'>
+              <div className={isMobile ? '' : 'ml-3'}>
+                <p className='lg:text-4xl text-3xl text-black tracking-tight font-extrabold mb-2 text-center-mobile'>Join the crew</p>
+                <p className='font-gray text-xl text-center-mobile'>Help us develop curriculum, teach students, and build the program.</p>
+              </div>
+              <a href="https://forms.gle/AQ4fv3vkMVr5GjL28" target="__blank" className='flex justify-center items-center hover:no-underline'>
+                <button
+                  style={{width: isMobile ? '100%' : ''}}
+                  className={`px-5 h-14 text-xl text-white bg-[#378805] hover:bg-[#2A6804] font-semibold rounded-xl ${isMobile ? 'mt-4 -mb-2' : ''}`}
+                >
+                  Sign up
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        {/* Experiences Slideshow */}
+        <div className='py-3 mx-2 my-20'>
+          <p className={`text-center lg:text-4xl text-3xl font-extrabold tracking-tight my-5 mx-3`}>Experiences Slideshow</p>
+          <div className='flex justify-center'>
+            <iframe style={{ width: '900px', height: isMobile ? '300px' : '507px' }} className='rounded-xl lg:shadow-xl shadow-gray cursor-pointer' src="https://www.youtube.com/embed/Yg4NB3fpN-w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
         
-        <div id="how-it-works" className='py-24 flex justify-center items-center'>
+        <div id="how-it-works" className='pt-16 flex justify-center items-center'>
             <form className='text-center p-5' onSubmit={sendEmail}>
-              <p className={`text-center lg:text-5xl text-4xl font-extrabold tracking-tight`}>Got questions?</p>
+              <p className={`text-center lg:text-4xl text-3xl font-extrabold tracking-tight`}>Got questions?</p>
               <div className='lg:flex lg:justify-center mt-8 mx-2'>
                   <input placeholder='Name' name="name" type="text" className={`h-14 rounded-md border-light-gray-3 border-2 px-3 mt-3 lg:mr-3 md:mr-3 ${isMobile ? '-ml-2' : ''}`} style={{width: isMobile ? '105%' : '300px'}} />
                   <input placeholder='Email' type="text" name="email" className={`h-14 rounded-md border-light-gray-3 border-2 px-3 mt-3 lg:mr-3 md:mr-3 ${isMobile ? '-ml-2' : ''}`} style={{width: isMobile ? '105%' : '300px'}} />
@@ -493,25 +560,6 @@ export default function LandingPage() {
           </form>
         </div>
 
-      </div>
-
-      <div className={`flex justify-center items-center pb-16 ${isMobile ? 'px-3' : ''}`} id="beta">
-        <div className='lg:px-5 px-3 py-8 -mt-20 bg-[#a3eb76]-0 lg:w-8/12 rounded-xl shadow shadow-light-gray-3 bg-light-gray'>
-          <div className='lg:flex lg:justify-between lg:mr-12'>
-            <div className={isMobile ? '' : 'ml-3'}>
-              <p className='lg:text-4xl text-3xl text-black tracking-tight font-extrabold mb-2 text-center-mobile'>Join the crew</p>
-              <p className='font-gray text-xl text-center-mobile'>Help us develop curriculum, teach students, and build the program.</p>
-            </div>
-            <a href="https://forms.gle/AQ4fv3vkMVr5GjL28" target="__blank" className='flex justify-center items-center hover:no-underline'>
-              <button
-                style={{width: isMobile ? '100%' : ''}}
-                className={`px-5 h-14 text-xl text-white bg-[#378805] hover:bg-[#2A6804] font-semibold rounded-xl ${isMobile ? 'mt-4 -mb-2' : ''}`}
-              >
-                Sign up
-              </button>
-            </a>
-          </div>
-        </div>
       </div>
 
       {/* Footer */}
